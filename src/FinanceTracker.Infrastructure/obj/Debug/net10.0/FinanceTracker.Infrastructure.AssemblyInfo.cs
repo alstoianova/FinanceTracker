2778650f687e72b63595a8294273677de6b568d8
Aliona Stoianova <alstoianova@icloud.com>
2026-05-14 18:51:06 +0300

    feat: add transaction balance tracking

--- src/FinanceTracker.Infrastructure/obj/Debug/net10.0/FinanceTracker.Infrastructure.AssemblyInfo.cs
+++ src/FinanceTracker.Infrastructure/obj/Debug/net10.0/FinanceTracker.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceTracker.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf1795ce99082a3aa6c2a92c9c00c48c5829331")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+402a69da8542ce7003ae88e75ea141a060207ff3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceTracker.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceTracker.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
