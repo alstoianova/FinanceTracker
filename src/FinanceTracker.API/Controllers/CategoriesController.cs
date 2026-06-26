2987bbecafce2317f7545b763f7872cb5aea4064
Aliona Stoianova <alstoianova@icloud.com>
2026-06-26 15:37:51 +0300

    Add CRUD operations for Categories

--- src/FinanceTracker.API/Controllers/CategoriesController.cs
+++ src/FinanceTracker.API/Controllers/CategoriesController.cs
@@ -1,3 +1,4 @@
+using FinanceTracker.Domain.Entities;
 using FinanceTracker.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,11 +16,52 @@ public class CategoriesController : ControllerBase
         _db = db;
     }
 
+    // GET: /Categories
     [HttpGet]
     public async Task<IActionResult> Get()
     {
         var categories = await _db.Categories.ToListAsync();
-
         return Ok(categories);
+    }
+
+    // POST: /Categories
+    [HttpPost]
+    public async Task<IActionResult> Create(Category category)
+    {
+        _db.Categories.Add(category);
+        await _db.SaveChangesAsync();
+
+        return Ok(category);
+    }
+
+    // PUT: /Categories/{id}
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(Guid id, Category updatedCategory)
+    {
+        var category = await _db.Categories.FindAsync(id);
+
+        if (category == null)
+            return NotFound("Category not found");
+
+        category.Name = updatedCategory.Name;
+
+        await _db.SaveChangesAsync();
+
+        return Ok(category);
+    }
+
+    // DELETE: /Categories/{id}
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var category = await _db.Categories.FindAsync(id);
+
+        if (category == null)
+            return NotFound("Category not found");
+
+        _db.Categories.Remove(category);
+        await _db.SaveChangesAsync();
+
+        return Ok("Category deleted successfully");
     }
 }
